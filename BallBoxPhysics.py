d721e049a947ed94c463e751949bb30ac19ad467
jakefowler <46506569+jakefowler@users.noreply.github.com>
2019-04-07 16:05:10 -0600

    Added more balls in the box

--- BallBoxPhysics.py
+++ BallBoxPhysics.py
@@ -1,19 +1,28 @@
 from vpython import *
+import random
+
+# box: object
+# size: takes input in vector form (x, y, z)
+boxColor = color.blue
+wallR = box(pos = vector(5, 0, 0), color = boxColor, size = vector(0.2, 10, 10))
+wallL = box(pos = vector(-5, 0, 0), color = boxColor, size = vector(0.2, 10, 10))
+wallT = box(pos = vector(0, 5, 0), color = boxColor, size = vector(10, 0.2, 10))
+wallBottom = box(pos = vector(0, -5, 0), color = boxColor, size = vector(10, 0.2, 10))
+wallBack = box(pos = vector(0, 0, -5), color = boxColor, size = vector(10, 10, 0.2))
+wallFront = box(pos = vector(0, 0, 5), color = boxColor, opacity = 0.3, size = vector(10, 10, 0.2))
+
 # pos: for the center position of the object. Takes input in vector form (x, y, z)
 # radius: the sphere radius
 # color: color of the object
 ball = sphere(pos = vector(-3, 0, 0), radius = 0.5, color = color.red)
-# box: object
-# size: takes input in vector form (x, y, z)
-boxColor = color.blue
-wallR = box(pos = vector(5, 0, 0), color = color.blue, size = vector(0.2, 10, 10))
-wallL = box(pos = vector(-5, 0, 0), color = color.blue, size = vector(0.2, 10, 10))
-wallT = box(pos = vector(0, 5, 0), color = color.blue, size = vector(10, 0.2, 10))
-wallBottom = box(pos = vector(0, -5, 0), color = color.blue, size = vector(10, 0.2, 10))
-wallBack = box(pos = vector(0, 0, -5), color = color.blue, size = vector(10, 10, 0.2))
-wallFront = box(pos = vector(0, 0, 5), color = color.blue, opacity = 0.3, size = vector(10, 10, 0.2))
-
 ball.velocity = vector(2, 1, 1.5)
+objects = [ball]
+
+for i in range(100):
+    objects.append(ball.clone(pos=vector(random.uniform(-4, 4), random.uniform(-4, 4), random.uniform(-4, 4)),
+                                color=vector(random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)))) 
+    objects[i+1].velocity = vector(random.uniform(-4, 4), random.uniform(-4, 4), random.uniform(-4, 4))
+
 # Define time interval
 t = 0 # time tracker
 deltat = 0.05 # increments
@@ -24,12 +33,12 @@ bufferSpace = ball.radius + wallR.size.x
 
 while t < 100:
     rate(50)
-    if ball.pos.x > (wallR.pos.x - bufferSpace) or ball.pos.x < (wallL.pos.x + bufferSpace):
-        ball.velocity.x = -ball.velocity.x
-    if ball.pos.y > (wallT.pos.y - bufferSpace) or ball.pos.y < (wallBottom.pos.y + bufferSpace):
-        ball.velocity.y = -ball.velocity.y
-    if ball.pos.z > (wallFront.pos.z - bufferSpace) or ball.pos.z < (wallBack.pos.z + bufferSpace):
-        ball.velocity.z = -ball.velocity.z
-    ball.pos = ball.pos + ball.velocity * deltat
+    for i in objects:
+        if i.pos.x > (wallR.pos.x - bufferSpace) or i.pos.x < (wallL.pos.x + bufferSpace):
+            i.velocity.x = -i.velocity.x
+        if i.pos.y > (wallT.pos.y - bufferSpace) or i.pos.y < (wallBottom.pos.y + bufferSpace):
+            i.velocity.y = -i.velocity.y
+        if i.pos.z > (wallFront.pos.z - bufferSpace) or i.pos.z < (wallBack.pos.z + bufferSpace):
+            i.velocity.z = -i.velocity.z
+        i.pos = i.pos + i.velocity * deltat
     t = t + deltat # update time
-    # Trying out github push
