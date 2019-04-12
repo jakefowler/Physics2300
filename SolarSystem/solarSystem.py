b06b8c59fcfd4046122bbac55e8e640d9de6be23
jakefowler <46506569+jakefowler@users.noreply.github.com>
2019-04-11 21:26:09 -0600

    Update solarSystem.py

--- SolarSystem/solarSystem.py
+++ SolarSystem/solarSystem.py
@@ -5,10 +5,10 @@ import argparse
 import csv
 import pandas as pd
 
-G = 6.6738e-11          # Newton's Gravitational Contant 
+G = 6.67408e-11          # Newton's Gravitational Contant 
 au = 1.496e+8 # au in kilometers
 sizeScale = 1000
-dt = 6.3e-12
+dt = 6.3e-4
 
 
 def readFile(filename):
@@ -43,27 +43,49 @@ def createPlanets(initPos, initVel, masses):
     
     for i in range(len(masses)):
         planets.append(sphere(pos=vector(initPos[i][0], initPos[i][1], initPos[i][2]), radius=sizeScale*radius[i]/au, 
-                        mass=masses[i], velocity=vector(initVel[i][0], initVel[i][1], initVel[i][2]), texture=textures.earth))
-        #planets[2].texture=textures.earth
+                        mass=masses[i], velocity=vector(initVel[i][0], initVel[i][1], initVel[i][2])))
+    planets[0].color = vector(0.5, 0.5, 0.5) # mercury
+    planets[0].texture = textures.rough 
+    planets[1].color = vector(1, 0.8, 0.4) # venus
+    planets[1].texture = textures.rock
+    planets[2].texture=textures.earth # earth
+    planets[3].color = vector(0.8, 0.1, 0.1) # mars
+    planets[3].texture = textures.stones
+    planets[4].texture = textures.wood_old # jupiter
+    planets[4].color = vector(0.7, 0.8, 0.7)
+    planets[5].color = vector(1, 0.9, 0.7) # saturn
+    planets[5].texture = textures.wood
+    planets[6].color = vector(0.7, 0.8, 0.8) # uranus
+    planets[7].color = vector(0.7, 0.8, 1) # neptune
+    planets[7].texture = textures.metal
+    planets[8].color = vector(0.7, 0.6, 0.5) # pluto
+
     return planets
 
-def simulateOrbitEulers(objects, initVel, masses):
+def simulateOrbitEulers(objects):
     """
     Function to calculate next position of all the objects and draw it
+    :param objects: list that hods all of the vpython spheres
+    :returns: nothing
     """
     while True:
+        rate(100)
         for i in objects:
-            rate(100)
             i.acceleration = vector(0,0,0)
             for j in objects:
                 if i != j:
-                    dist = j.pos - i.pos
-                    i.acceleration = i.acceleration + G * j.mass * dist / mag(dist)**3
+                    dist = (j.pos - i.pos) * au
+                    i.acceleration = i.acceleration + G * j.mass * dist / mag(dist)**3 * 0.000577548
         for i in objects:
             i.velocity = i.velocity + i.acceleration*dt
             i.pos = i.pos + i.velocity * dt
 
 def simulateOrbitFrog(objects):
+    """
+    Function that simulates the orbit of the planets using the leap frog method
+    :param objects: list that holds all of the vpython spheres 
+    :returns: nothing
+    """
     firstStep = 0
     while True:
         rate(1000)
@@ -97,12 +119,12 @@ def main():
     initPos, initVel, masses = readFile(filename)
 
     objects = createPlanets(initPos, initVel, masses)
-    sun = sphere(pos=vector(0, 0, 0), radius=(sizeScale/100)*695510/au, color=color.yellow, mass=1.99e+30, velocity=vector(0, 0, 0)) # scaled the sun down
+    sun = sphere(pos=vector(0, 0, 0), radius=(sizeScale/100)*695510/au, color=color.yellow, texture=textures.flower, mass=1.9891e+30, velocity=vector(0, 0, 0)) # scaled the sun down
     objects.append(sun)
     initVel = list(initVel).append([0, 0, 0]) # add the sun to the velocity list
     masses = list(masses).append(1.99e+30) # add the mass of the sun to the array
 
-    simulateOrbitFrog(objects)
+    simulateOrbitEulers(objects)
 
 
 
